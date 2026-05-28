32e921e3fef3e11c7ff8a4f3201f40b8d3da1362
Leo nispe <leomoisesnisperuzaamaya7@gmail.com>
2026-05-28 12:52:14 -0500

    l

--- Proyecto Colaborativo de equipos tcc/client/src/lib/socket.ts
+++ Proyecto Colaborativo de equipos tcc/client/src/lib/socket.ts
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:8000"); 
+export const socket = io("http://ec2-100-53-3-92.compute-1.amazonaws.com:8000"); 
 // ⚠️ pon tu URL real backend
