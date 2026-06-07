d52b8c27a06a1f831c9771113bbf7ee1fbcf4394
Leo nispe <leomoisesnisperuzaamaya7@gmail.com>
2026-06-07 08:33:50 -0500

    areglando problemas de typescript

--- Proyecto Colaborativo de equipos tcc/backend/src/controllers/member.controller.ts
+++ Proyecto Colaborativo de equipos tcc/backend/src/controllers/member.controller.ts
@@ -8,16 +8,25 @@ import { getWorkspaceMembersService } from "../services/member.service";
 
 export const joinWorkspaceController = asyncHandler(
   async (req: Request, res: Response) => {
-    const inviteCode = z.string().parse(req.params.inviteCode);
-    const userId = req.user?._id;
-
-    const { workspaceId, role } = await joinWorkspaceByInviteService(
-      userId,
-      inviteCode
+    const inviteCode = z.string().parse(
+      req.params.inviteCode
     );
 
-    return res.status(HTTPSTATUS.OK).json({
-      message: "Successfully joined the workspace",
+    const userId = String(
+      req.user?._id
+    );
+
+    const { workspaceId, role } =
+      await joinWorkspaceByInviteService(
+        userId,
+        inviteCode
+      );
+
+    return res.status(
+      HTTPSTATUS.OK
+    ).json({
+      message:
+        "Successfully joined the workspace",
       workspaceId,
       role,
     });
